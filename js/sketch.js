ed708368fdabf086bc9f94fbca533cbdd3f24b02
Lillian <lillian@resnet-113-143.ucdavis.edu>
2016-01-28 14:36:04 -0800

    recommiting changes

--- js/sketch.js
+++ js/sketch.js
@@ -1,13 +1,11 @@
 $(document).ready(function() {
   //console.log("ready");
-
-$('#hideAll').click(function() {
-   $('#entry').hide();
-   return false; 
-});
     
+$('#hideAll').click(function() {
+   $('section#entry h2, section#entry img, section#entry ul').hide();
+
 $('#showAll').click(function() {
-    $('#entry').show(); 
+    $('section#entry h2, section#entry img, section#entry ul').show();
 
     return false; 
 });  

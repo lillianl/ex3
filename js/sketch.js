f897ef680e688868fece231c1050a20e0e933c7d
Lillian <lillian@resnet-113-143.ucdavis.edu>
2016-01-28 14:10:53 -0800

    initial commit

---
+++ js/sketch.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  //console.log("ready");
+
+$('#hideAll').click(function() {
+   $('#entry').hide();
+   return false; 
+});
+    
+$('#showAll').click(function() {
+    $('#entry').show(); 
+
+    return false; 
+});  
+    
+    
+}
+                  

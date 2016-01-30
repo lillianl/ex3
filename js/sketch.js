0ff877a4253dcbdd035de42c280ef42e2b4a806d
Lillian <lillian@Lillians-MacBook-Pro.local>
2016-01-29 20:21:41 -0800

    fixed errors

--- js/sketch.js
+++ js/sketch.js
@@ -1,15 +1,31 @@
-$(document).ready(function() {
+$(document).ready(function(){
   //console.log("ready");
     
-$('#hideAll').click(function() {
-   $('section#entry h2, section#entry img, section#entry ul').hide();
+    $('#hideAll').click(function() {
+       $('section.entry').hide();
 
-$('#showAll').click(function() {
-    $('section#entry h2, section#entry img, section#entry ul').show();
+        return false;
+    });
 
-    return false; 
-});  
+
+    $('#showAll').click(function() {
+        $('section.entry').show();
+        return false;        
+
+    });
     
-    
-}
-                  
+// a for loop for each category. called a function with two parameters 
+//arguments to .each function is an array and a callback. The indexInArray needs to be there even if it's not used
+
+    var categories=["transit","food","work","panic"];
+    $.each(categories,function(i,category){
+        $('a#'+category).click(function() {
+            $('section.entry').hide();  
+            $('section.entry.'+category).show();
+            return false;
+        });
+
+    });
+
+});
+
